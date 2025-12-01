a56c77cfe0528fb673a00fc17170804e6a0ac683
Marco Valeri <marcovaleri@mac.home>
2025-12-01 20:46:54 +0000

    Add tangerine font

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="bg-black text-white p-8 text-center">
-            <h2 className="text-6xl">Marco Valeri</h2>
+            <h2 className="text-6xl" style={{ fontFamily: 'var(--font-tangerine)' }}>Marco Valeri</h2>
         </header>
     )
 }
