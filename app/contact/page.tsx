427dbecd962d4701fc7955876fb800363e8255e9
Marco Valeri <marcovaleri@mac.home>
2026-02-18 16:21:09 +0000

    Create ContactPage

---
+++ app/contact/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header/Header"
+import MainLayout from "../components/MainLayout/MainLayout"
+import Nav from "../components/Nav/Nav"
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Nav />
+            <Header />
+            <MainLayout>
+
+            </MainLayout>
+        </div>
+    )
+}
+
+export default ContactPage;
